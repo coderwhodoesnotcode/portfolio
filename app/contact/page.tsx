20b90c6b3f7dba0197e980153b1ddf48d386b944
coderwhodoesnotcode <fawadtwopointo@gmail.com>
2026-09-18 17:06:00 +0500

    newded

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,22 @@
 export default function Contact() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-10 text-center">
-      <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
-      <p className="text-lg text-gray-600 mb-6">
+      <h1 className="text-4xl font-bold mb-5">Contact Me</h1>
+      <p className="text-lg text-gray-600 mb-5">
         Web Developer | Next.js Enthusiast
       </p>
-      <div className="flex gap-4">
+      <div className="flex gap-5">
         <a
           href="#projects"
-          className="px-4 py-2 bg-black text-white rounded-lg"
+          className="px-4 py-3 bg-black text-white rounded-lg"
         >
-          WhatsApp
+          konianppp
         </a>
         <a
           href="#contact"
           className="px-4 py-2 border border-black rounded-lg"
         >
-          Emaillllllllllllll
+          Emailppppp
         </a>
       </div>
     </main>
