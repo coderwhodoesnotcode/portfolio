9f29fc3e875b2f46562f3fc1e1ea1b1df54807cb
coderwhodoesnotcode <fawadtwopointo@gmail.com>
2026-09-18 16:55:47 +0500

    newded

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -16,7 +16,7 @@ export default function Contact() {
           href="#contact"
           className="px-4 py-2 border border-black rounded-lg"
         >
-          Email
+          Emaillllllllllllll
         </a>
       </div>
     </main>
